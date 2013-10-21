9938575ea72c44a50866ce95e99a1723bbc792f8
jwosty <jwosty@users.noreply.github.com>
2013-10-20 21:02:46 -0600

    Created F# MonoGame project and fixed DLL references

---
+++ CircusMaximus/Main.fs
@@ -0,0 +1,22 @@
+namespace CircusMaximus
+open MonoMac.AppKit
+open MonoMac.Foundation
+
+type AppDelegate() = 
+    inherit NSApplicationDelegate()
+    
+    override x.FinishedLaunching(notification) =
+        let game = new CircusMaximusGame()
+        game.Run()
+    
+    override x.ApplicationShouldTerminateAfterLastWindowClosed(sender) =
+        true
+ 
+module main =         
+    [<EntryPoint>]
+    let main args =
+        NSApplication.Init ()
+        using (new NSAutoreleasePool()) (fun n -> 
+            NSApplication.SharedApplication.Delegate <- new AppDelegate()
+            NSApplication.Main(args) )
+        0
