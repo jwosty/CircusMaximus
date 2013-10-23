14d93dce99ccb65b74d9d4bfcb9da9132d9c166c
jwosty <jwosty@users.noreply.github.com>
2013-10-22 21:59:14 -0600

    Renamed occurences of 'x' to 'this' and added a call to Player.loadContent

--- CircusMaximus/Game1.fs
+++ CircusMaximus/Game1.fs
@@ -10,35 +10,33 @@ open Microsoft.Xna.Framework.Content
 open Microsoft.Xna.Framework.Media
 
     /// Default Project Template
-    type CircusMaximusGame() as x =
+    type CircusMaximusGame() as this =
         inherit Game()
-        let graphics = new GraphicsDeviceManager(x)
+        let graphics = new GraphicsDeviceManager(this)
         let mutable spriteBatch = Unchecked.defaultof<_>
-        let mutable logoTexture = Unchecked.defaultof<_>
+        let mutable playerTexture = Unchecked.defaultof<_>
         do
-          x.Content.RootDirectory <- "Content"
+          this.Content.RootDirectory <- "Content"
           graphics.IsFullScreen <- false
      
         /// Overridden from the base Game.Initialize. Once the GraphicsDevice is setup,
         /// we'll use the viewport to initialize some values.
-        override x.Initialize() = base.Initialize()
+        override this.Initialize() = base.Initialize()
 
         /// Load your graphics content.
-        override x.LoadContent() =
+        override this.LoadContent() =
             // Create a new SpriteBatch, which can be use to draw textures.
-            spriteBatch <- new SpriteBatch (graphics.GraphicsDevice)
-            
-            // TODO: use this.Content to load your game content here eg.
-            //logoTexture <- x.Content.Load<_>("logo")
+            spriteBatch <- new SpriteBatch(graphics.GraphicsDevice)
+            playerTexture <- Player.loadContent this.Content
 
         /// Allows the game to run logic such as updating the world,
         /// checking for collisions, gathering input, and playing audio.
-        override x.Update ( gameTime:GameTime) =
+        override this.Update ( gameTime:GameTime) =
             // TODO: Add your update logic here                 
             base.Update (gameTime)
 
         /// This is called when the game should draw itself. 
-        override x.Draw (gameTime:GameTime) =
+        override this.Draw (gameTime:GameTime) =
             // Clear the backbuffer
             graphics.GraphicsDevice.Clear (Color.CornflowerBlue)
 
