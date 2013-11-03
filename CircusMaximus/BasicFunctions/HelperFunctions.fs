942c36f8a9a3268659959d9931ea321c237bb0e3
jwosty <jwosty@users.noreply.github.com>
2013-11-02 23:53:30 -0600

    Wrote a cross product function

--- CircusMaximus/BasicFunctions/HelperFunctions.fs
+++ CircusMaximus/BasicFunctions/HelperFunctions.fs
@@ -4,6 +4,9 @@ open Microsoft.Xna.Framework
 
 let inline (@@) a b = new Vector2(float32 a, float32 b)
 let inline (@~) a b = new Nullable<_>(new Vector2(float32 a, float32 b))
+// Vector2 cross product
+let cross (a: Vector2) (b: Vector2) = (a.X * b.X) - (a.Y * b.Y)
+
 /// Exclusive 'between' operator
 let (><) x (a, b) = (x > a && x < b) || (x < a && x > b)
 /// Inclusive 'between' operator
