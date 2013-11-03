aa37f1b480d9107f447e6a66295c777ddea57372
jwosty <jwosty@users.noreply.github.com>
2013-11-03 14:58:33 -0700

    Shifted indices again

--- CircusMaximus/BasicFunctions/BoundingBox2D.fs
+++ CircusMaximus/BasicFunctions/BoundingBox2D.fs
@@ -27,10 +27,10 @@ type BoundingBox2D =
     member this.Corners =
       let origin, direction = this.Center, this.Direction
       // Offsets from the center
-      [ -this.HalfWidth @@ this.HalfHeight ;
-        -this.HalfWidth @@ -this.HalfHeight;
-         this.HalfWidth @@ -this.HalfHeight;
-         this.HalfWidth @@ this.HalfHeight ]
+      [  this.HalfWidth @@ -this.HalfHeight;
+         this.HalfWidth @@  this.HalfHeight ;
+        -this.HalfWidth @@  this.HalfHeight ;
+        -this.HalfWidth @@ -this.HalfHeight]
         //]
       // Rotate the points around the center by applying a rotation matrix, and ofsetting by the origin
         |> List.map (fun v -> Vector2.Transform(v, Matrix.CreateRotationZ(float32 direction)) + origin)
