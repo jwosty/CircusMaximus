7e3acf18e493b9e20ba99947c6dc0ac6787cca2a
jwosty <jwosty@users.noreply.github.com>
2013-11-05 23:14:21 -0700

    Refactored code to use LineSegment operator -+- again

--- CircusMaximus/Collision/Collision.fs
+++ CircusMaximus/Collision/Collision.fs
@@ -30,36 +30,12 @@ let combineResultsPair a b =
 /// Combines multiple collision results into one, typically all of the same object
 let combineResults results = List.reduce combineResultsPair results
 
-/// Tests if two line segments intersect (maths come from http://stackoverflow.com/a/565282/1231925)
-let testIntersection_LineSeg_LineSeg ((p, pr): LineSegment) ((q, qs): LineSegment) =
-  let qp = q - p
-  let r = pr - p
-  let s = qs - q
-  
-  let qmpxr = cross qp r
-  let qmpxs = cross qp s
-  let rxs = cross r s
-  
-  if qmpxr = 0.0f then
-    (((q.X - p.X < 0.0f) <> (q.X - pr.X < 0.0f))
-      || ((q.Y - p.Y < 0.0f) <> (q.Y - pr.Y < 0.0f))) |> twice
-  else
-  if rxs = 0.0f then
-    false, false
-  else
-    let rxsr = 1.0f / rxs
-    let t = qmpxs * rxsr
-    let u = qmpxr * rxsr
-    
-    (t >=< (0.0f, 1.0f) && u >=< (0.0f, 1.0f)) |> twice
-
-let collide_LineSeg_LineSeg a b =
-  testIntersection_LineSeg_LineSeg a b |> Tuple.t2Map Result_LR
+let collide_LineSeg_LineSeg a b = a -+- b |> twice |> Tuple.t2Map Result_LR
 
 /// Returns the indices of every edge that is intersecting the given line segment
 let collide_ORect_LineSeg (rect: OrientedRectangle) (seg: LineSegment) =
   let resultR, resultS =
-    Tuple.t4Map (fun edge -> testIntersection_LineSeg_LineSeg edge seg) rect.Edges
+    Tuple.t4Map (fun edge -> edge -+- seg |> twice) rect.Edges
       |> Tuple.t4Unzip2
   Result_BR(resultR), Result_LR(Tuple.t4Reduce (||) resultS)
 
@@ -70,14 +46,14 @@ let collide_ORect_ORect (a: OrientedRectangle) (b: OrientedRectangle) =
       |> Tuple.t4Map
           (fun aEdge ->
             b.Edges
-              |> Tuple.t4Map (fun bEdge -> testIntersection_LineSeg_LineSeg aEdge bEdge |> fst)
+              |> Tuple.t4Map ((-+-) aEdge)
               |> Tuple.t4Reduce (||))),
   Result_BR(
     b.Edges
       |> Tuple.t4Map
           (fun bEdge ->
             a.Edges
-              |> Tuple.t4Map (fun aEdge -> testIntersection_LineSeg_LineSeg bEdge aEdge |> fst)
+              |> Tuple.t4Map ((-+-) bEdge)
               |> Tuple.t4Reduce (||)))
 
 let collidePair a b =
@@ -86,13 +62,6 @@ let collidePair a b =
     | BoundingRectangle(a), BoundingRectangle(b) -> collide_ORect_ORect a b
     | BoundingRectangle(a), BoundingLineSegment(b) -> collide_ORect_LineSeg a b
     | BoundingLineSegment(a), BoundingRectangle(b) -> collide_ORect_LineSeg b a
-
-let nextIndex length i =
-  //let iLen = length objects
-  if i <= (length - 2) then
-    i + 1
-  else
-    0
 
 /// Calculates the intersections of a list of items. Not optimized at all yet
 let collideWorld objects =
