24c6259c5d0ae24f0b75ef139b5c3d3498ed1db0
jwosty <jwosty@users.noreply.github.com>
2013-11-19 20:47:52 -0700

    Wrote a pre-race stage that simply counts down from 3

--- CircusMaximus/GameElements/Game.fs
+++ CircusMaximus/GameElements/Game.fs
@@ -17,12 +17,11 @@ type CircusMaximusGame() as this =
   inherit Microsoft.Xna.Framework.Game()
   let graphics = new GraphicsDeviceManager(this)
   let mutable playerScreens = Unchecked.defaultof<_>
-
   //let mutable players =
   let mutable gameState =
     let x = 820.0f
-    MidRace(
-      MidRaceData(
+    PreRace(
+      PreRaceData(
         [
           x, 740.0f;
           x, 950.0f;
@@ -30,15 +29,17 @@ type CircusMaximusGame() as this =
           x, 1370.0f;
           x, 1580.0f;
         ] |> List.map (fun (x, y) -> Player.Moving(new State.Player.Moving(new OrientedRectangle(x@@y, 64.0f, 29.0f, 0.0), 0.0, Racetrack.center, None))),
-        0, 0))
-  
+        0))
   // 1st place, 2nd place, etc
   let mutable lastPlacing = 0
+  // A general-purpose sprite batch
+  let mutable generalBatch = Unchecked.defaultof<_>
   let mutable fontBatch = Unchecked.defaultof<_>
   let mutable pixelTexture = Unchecked.defaultof<_>
   let mutable playerTexture = Unchecked.defaultof<_>
   let mutable racetrackTextures = Unchecked.defaultof<_>
   let mutable font = Unchecked.defaultof<_>
+
   do
     this.Content.RootDirectory <- "Content"
 #if DEBUG
@@ -48,13 +49,23 @@ type CircusMaximusGame() as this =
 #endif
     graphics.PreferredBackBufferWidth <- GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width
     graphics.PreferredBackBufferHeight <- GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height
-
+  
+  member this.WindowRect = new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight)
+  member this.WindowDimensions = graphics.PreferredBackBufferWidth @@ graphics.PreferredBackBufferHeight
+  member this.WindowCenter = this.WindowDimensions * (0.5 @@ 0.5)
+  
   /// Overridden from the base Game.Initialize. Once the GraphicsDevice is setup,
   /// we'll use the viewport to initialize some values.
   override this.Initialize() =
     base.Initialize()
     this.IsMouseVisible <- true
-    playerScreens <- PlayerScreen.createScreens this.GraphicsDevice (match gameState with | MidRace raceData -> raceData.players.Length)
+    playerScreens <-
+      PlayerScreen.createScreens
+        this.GraphicsDevice
+        (match gameState with
+          | PreRace raceData -> raceData.players.Length
+          | MidRace raceData -> raceData.players.Length)
+    generalBatch <- new SpriteBatch(this.GraphicsDevice)
     fontBatch <- new SpriteBatch(this.GraphicsDevice)
   
   /// Load your graphics content.
@@ -74,24 +85,42 @@ type CircusMaximusGame() as this =
   
   /// This is called when the game should draw itself.
   override this.Draw(gameTime:GameTime) =
+    // Since the borders btwn the player screens are merely trimmed edges, they show through to the
+    // background and become whatever color the screen is cleared with
+    graphics.GraphicsDevice.Clear (Color.Black)
+    base.Draw (gameTime)
     match gameState with
+    | PreRace raceData ->
+      this.DrawScreens(raceData.players)
+      // Draw a dark overlay to indicate that the game hasn't started yet
+      generalBatch.Begin()
+      generalBatch.Draw(pixelTexture, this.WindowRect, new Color(Color.Black, 192))
+      generalBatch.End()
+      // Draw a countdown
+      this.FontBatchDo fontBatch
+        (fun (fb: SpriteBatch) ->
+          FlatSpriteFont.drawString
+            font fontBatch (State.Game.preRaceMaxCount - (raceData.timer / State.Game.ticksPerCount) |> string)
+            this.WindowCenter 8.0f Color.White (FlatSpriteFont.Center, FlatSpriteFont.Center))
     | MidRace raceData ->
-      // Since the borders btwn the player screens are merely trimmed edges, they show through to the
-      // background and become whatever color the screen is cleared with
-      graphics.GraphicsDevice.Clear (Color.Black)
-      base.Draw (gameTime)
-      // SamplerState.PointClamp disables anti-aliasing, which just looks horrible on scaled bitmap fonts
-      fontBatch.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp, null, null)
-      List.iteri2
-        (PlayerScreen.drawSingle
-          (fun (a, b) -> this.DrawRace(raceData.players, a, b)))
-          raceData.players playerScreens
-      fontBatch.End()
-      fontBatch.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp, null, null)
-      List.iter2 this.DrawHUD raceData.players playerScreens
-      fontBatch.End()
+      this.DrawScreens(raceData.players)
+      this.FontBatchDo fontBatch (fun fb -> List.iter2 (this.DrawHUD fb) raceData.players playerScreens)
   
-  member this.DrawRace(players, mainPlayer, ((sb, rect): PlayerScreen.PlayerScreen)) =
+  member this.DrawScreens(players) =
+    this.FontBatchDo fontBatch
+      (fun fb ->
+        List.iteri2
+          (PlayerScreen.drawSingle
+            (fun (a, b) -> this.DrawWorld(players, a, b)))
+            players playerScreens)
+  
+  member this.FontBatchDo (fb: SpriteBatch) predicate =
+    // SamplerState.PointClamp disables anti-aliasing, which just looks horrible on scaled bitmap fonts
+    fb.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp, null, null)
+    predicate fb |> ignore
+    fb.End()
+  
+  member this.DrawWorld(players, mainPlayer, ((sb, rect): PlayerScreen.PlayerScreen)) =
     for x in 0..9 do
       for y in 0..2 do
         Racetrack.drawSingle sb racetrackTextures.[x, y] x y
@@ -100,11 +129,11 @@ type CircusMaximusGame() as this =
 #endif
     List.iteri (fun i player -> Player.draw (sb, rect) player (i = mainPlayer) playerTexture font fontBatch pixelTexture) players
   
-  member this.DrawHUD player ((sb, rect): PlayerScreen.PlayerScreen) =
+  member this.DrawHUD fb player ((sb, rect): PlayerScreen.PlayerScreen) =
     match player with
     | Player.Moving player ->
         FlatSpriteFont.drawString
-          font fontBatch
+          font fb
           (sprintf "Flexus: %s" (MathHelper.Clamp(player.turns, 0, Int32.MaxValue) |> toRoman))
           (float32 rect.X + (float32 rect.Width / 2.0f) @@ rect.Y)
           3.0f Color.White (FlatSpriteFont.Center, FlatSpriteFont.Min)
@@ -112,7 +141,7 @@ type CircusMaximusGame() as this =
         | Some placing ->
             let color = match placing with | 1 -> Color.Gold | 2 -> Color.Orange | 3 -> Color.OrangeRed | _ -> Color.Gray
             FlatSpriteFont.drawString
-              font fontBatch
+              font fb
               (sprintf "Locus: %s" (toRoman placing))
               (float32 rect.X + (float32 rect.Width / 2.0f) @@ rect.Y + (24))
               3.0f color (FlatSpriteFont.Center, FlatSpriteFont.Min)
--- CircusMaximus/GameElements/GameStates.fs
+++ CircusMaximus/GameElements/GameStates.fs
@@ -7,6 +7,14 @@ open CircusMaximus.HelperFunctions
 open CircusMaximus.Extensions
 open CircusMaximus.Collision
 
+type PreRaceData =
+  struct
+    val players: Player.Player list
+    val timer: int
+    
+    new(players, timer) = { players = players; timer = timer }
+  end
+
 type MidRaceData =
   struct
     val players: Player.Player list
@@ -17,37 +25,46 @@ type MidRaceData =
   end
 
 type Game =
+  | PreRace of PreRaceData
   | MidRace of MidRaceData
+
+let preRaceTicks, preRaceMaxCount = 175, 3
+let ticksPerCount = float preRaceTicks / float preRaceMaxCount |> ceil |> int
 
 /// Update a gameState, returning an option of the new state; the Some case here represents that the game
 /// shall continue and None indicating that the whole game should stop
 let update gameState (keyboard: KeyboardState) (getGamepad: PlayerIndex -> _) =
-  match gameState with
+  if keyboard.IsKeyDown(Keys.Escape) then
+    None  // Indicate that we want to exit
+  else
+    match gameState with
+    | PreRace raceData ->
+      if raceData.timer >= preRaceTicks then
+        Some(MidRace(MidRaceData(raceData.players, 0, 0)))
+      else
+        Some(PreRace(PreRaceData(raceData.players, raceData.timer + 1)))
     | MidRace raceData ->
-        if keyboard.IsKeyDown(Keys.Escape) then
-          None  // Indicate that we want to exit
-        else
-          // A list of collision results (more like intersection results)
-          let collisions =
-            (Racetrack.collisionBounds
-              :: (raceData.players |> List.map Player.playerBB))
-              |> Collision.collideWorld
-          let lastPlacing = ref raceData.lastPlacing
-          // Update the players (collision and input)
-          let updatedPlayers =
-            List.mapi2
-              (fun i player collisionResult ->
-                let collision = match collisionResult with | Collision.Result_Poly(lines) -> lines | _ -> failwith "Bad player collision result; that's not supposed to happen... It's probably a bug!"
-                let otherPlayers = List.removeIndex i raceData.players // eww... better / more efficient way to do this?
-                let player, p =
-                  if i = 0 then Player.update (Player.getPowerTurnFromKeyboard keyboard) player collision raceData.lastPlacing (keyboard.IsKeyDown(Keys.Q)) Racetrack.center
-                  else
-                    let gamepad = getGamepad(enum <| i - 1)
-                    Player.update (Player.getPowerTurnFromGamepad gamepad) player collision raceData.lastPlacing (gamepad.Buttons.A = ButtonState.Pressed) Racetrack.center
-                // TODO: Not functional. Fix it!!
-                match p with | Some placing -> (lastPlacing := placing) | None -> ()
-                player)
-              raceData.players
-              (collisions |> List.tail)
-          // Return the new and improved game state
-          Some(MidRace(MidRaceData(updatedPlayers, raceData.timer + 1, !lastPlacing)))
+        // A list of collision results (more like intersection results)
+        let collisions =
+          (Racetrack.collisionBounds
+            :: (raceData.players |> List.map Player.playerBB))
+            |> Collision.collideWorld
+        let lastPlacing = ref raceData.lastPlacing
+        // Update the players (collision and input)
+        let updatedPlayers =
+          List.mapi2
+            (fun i player collisionResult ->
+              let collision = match collisionResult with | Collision.Result_Poly(lines) -> lines | _ -> failwith "Bad player collision result; that's not supposed to happen... It's probably a bug!"
+              let otherPlayers = List.removeIndex i raceData.players // eww... better / more efficient way to do this?
+              let player, p =
+                if i = 0 then Player.update (Player.getPowerTurnFromKeyboard keyboard) player collision raceData.lastPlacing (keyboard.IsKeyDown(Keys.Q)) Racetrack.center
+                else
+                  let gamepad = getGamepad(enum <| i - 1)
+                  Player.update (Player.getPowerTurnFromGamepad gamepad) player collision raceData.lastPlacing (gamepad.Buttons.A = ButtonState.Pressed) Racetrack.center
+              // TODO: Not functional. Fix it!!
+              match p with | Some placing -> (lastPlacing := placing) | None -> ()
+              player)
+            raceData.players
+            (collisions |> List.tail)
+        // Return the new and improved game state
+        Some(MidRace(MidRaceData(updatedPlayers, raceData.timer + 1, !lastPlacing)))
