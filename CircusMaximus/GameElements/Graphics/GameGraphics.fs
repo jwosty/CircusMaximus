236e62d094a5fd5dfb9c8b39025f5c934a5e1dca
jwosty <jwosty@users.noreply.github.com>
2014-01-04 17:54:13 -0700

    Fixed a typo that somehow made it in the last commit

--- CircusMaximus/GameElements/Graphics/GameGraphics.fs
+++ CircusMaximus/GameElements/Graphics/GameGraphics.fs
@@ -46,7 +46,7 @@ let drawScreens playerScreens assets (fontBatch: SpriteBatch) players =
 let drawGame windowCenter (windowRect: Rectangle) playerScreens assets (generalBatch: SpriteBatch) (fontBatch: SpriteBatch) (game: Game) =
   match game.gameState with
   | MainMenu mainMenu ->
-    MainMenuGraphics.draw screen fontBatch generalBatch assets
+    MainMenuGraphics.draw mainMenu fontBatch generalBatch assets
   
   | Race race ->
     match race.raceState with
