208414ee487fcd308154276f0e928d559a23b624
jwosty <jwosty@users.noreply.github.com>
2014-01-20 15:49:43 -0700

    Added comments to some of the type aliases

--- CircusMaximus/GameElements/State/Player/Player.fs
+++ CircusMaximus/GameElements/State/Player/Player.fs
@@ -10,9 +10,13 @@ open CircusMaximus.HelperFunctions
 open CircusMaximus.Input
 open CircusMaximus.Collision
 
+/// A velocity, in percentage (where 0 = 0% and 1 = 100%) of a top speed
 type Velocity = float
+/// A player's finishing place in the race
 type Placing = int
+/// A length of time
 type Duration = int
+/// A taunt contains the string to display as well as the amount of time it lasts for
 type Taunt = string * Duration
 
 type MotionState = Moving of Velocity | Crashed
